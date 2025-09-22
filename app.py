c1b3f8a657c3d06de8c680048e268434fed82c06
BarurePriyanka <157453246+BarurePriyanka@users.noreply.github.com>
2025-09-22 17:43:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Hello from Sandip Ompraksh Madde!")
+st.write("This is my portfolio")
+
+number = st.slider("score my resume", 1, 10, 5)
+st.write("You selected:", number)
+st.title("Process Engineer at Enpro")
+st.header("B.tech Graduate specialized in Chemical engineering")
+st.subheader("With 7 years od proccess enginner experience")
+st.text("I am a dedicated and enthusiastic professional with a strong foundation in mechanical engineering and hands-on experience in lube oil skid systems, dry gas seal support systems, and auxiliary equipment for centrifugal compressors. My academic journey and projects in Python-based facial expression recognition and heart disease prediction have further strengthened my problem-solving mindset and adaptability to modern technologies. I am passionate about continuous learning, innovation, and applying my skills to create impactful solutions.With strong communication and teamwork abilities, I aim to contribute effectively to organizational goals while growing personally and professionally.")
+st.markdown("**Looking forward to hear from you**")
